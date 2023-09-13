560cc0d609f93912c20c606c141e601258a02ea7
Mohammad Anar <ai.anarul4232@gmail.com>
2023-09-13 07:23:33 +0600

    some modification on small screen

--- src/Bookmarks/Bookmarks.jsx
+++ src/Bookmarks/Bookmarks.jsx
@@ -4,11 +4,11 @@ import Bookmark from '../Bookmark/Bookmark';
 
 const Bookmarks = ({bookmarks, readingTime}) => {
     return (
-        <div className="sticky top-0">
+        <div >
             <h1 className="text-2xl font-semibold bg-gray-200 py-4 px-8 rounded-lg text-purple-600">Spent time on read: {readingTime} Hrs</h1>
-            <div className=" bg-gray-200 p-8 rounded-lg mt-4">
+            <div className="  bg-gray-200 p-8 rounded-lg mt-4">
              <h1 className="text-2xl font-semibold mb-8 text-purple-600">Book marked: {bookmarks.length} </h1>
-             <div>
+             <div className='h-[150px] overflow-y-scroll'>
                 {
                     bookmarks.map((bookmark, idx) => <Bookmark 
                     key={idx}
